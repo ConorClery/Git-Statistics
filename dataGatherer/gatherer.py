38be369df6fae2bb6ad1dfd85fc31e42779d3abc
cleryc <cleryc@tcd.ie>
2018-11-13 13:20:46 +0000

    Program now pulls apart programming language data from a specified amount of irish users and writes them to a firebase database

--- dataGatherer/gatherer.py
+++ dataGatherer/gatherer.py
@@ -1,24 +1,48 @@
-# importing the requests library
 import requests
 import sys
 import getpass
 import json
 from firebase import firebase
 
-
-def processData( data ):
-    print(data["bio"])
-    str = data["bio"]
-    str = str.lower()
-    if "student" and "trinity" and "dublin" in str:
-        return True
-
-
-
 firebaseEndpoint = 'https://swengdb.firebaseio.com/'
+firebase = firebase.FirebaseApplication(firebaseEndpoint, None)
 github = 'https://api.github.com/'
 user = ''
 pw = ''
+repoData = []
+id = 0
+
+
+def processRepoData( data ):
+    num = 0
+    dataLength = len(data["items"])
+    for element in data["items"]:
+        print(element["repos_url"])
+        repUrl = element["repos_url"]
+        repoData = getRepoData( repUrl )
+        for repo in repoData:
+            getAndPostRepoLanguageData(repo["owner"]["login"], repo["name"])
+        num = num + 1
+        woops = str(num) + '/' + str(dataLength)
+        print("Current User Progress:", woops)
+    return
+
+def getRepoData( repUrl ):
+    PARAMS = {'Content-Type':'application/json'}
+    r = requests.get(url = repUrl, params = PARAMS, auth =( user, pw ))
+    return r.json()
+
+def getAndPostRepoLanguageData(owner, name):
+    langUrl = github + 'repos/' + owner + '/' + name +'/languages'
+    PARAMS = {'Content-Type':'application/json'}
+    r = requests.get(url = langUrl, params = PARAMS, auth =( user, pw ))
+    print("posting to db...")
+    id = id + 1
+    fireData = {'id' : id, 'progLangs' : r.json()}
+    sent = json.dumps(fireData)
+    result = firebase.post("/languageData", sent)
+    return
+
 user = input('Enter in Github username (or skip for limited querires): ')
 if user != '':
     pw = getpass.getpass('Enter in Github password: ')
@@ -26,13 +50,17 @@ if user != '':
 print('Number of endpoints to query = ', (len(sys.argv) - 1), '\nBeginning given requests...\n')
 for i, endpoint in enumerate(sys.argv):
     if i > 0:
-        print('\nURL', i, ': ', github, endpoint)
         URL = github + endpoint
+        print('\nURL', i, ': ', URL)
         # defining a params dict for the parameters to be sent to the API
-        PARAMS = {'Content-Type':'application/json', 'location':'ireland'}
+        PARAMS = {'Content-Type':'application/json'}
         # sending get request and saving the response as response object
         j = 0
-        while j < 10:
+        limit = 1
+        prog = ''
+        while j < limit:
+            prog = str(j) + '/' + str(limit)
+            print('Overall Progress:', prog)
             page = '&page=' + str(j)
             r = requests.get(url = URL + (page), params = PARAMS, auth =( user, pw ))
             print(r.status_code)
@@ -40,13 +68,10 @@ for i, endpoint in enumerate(sys.argv):
             if contentType[0] == 'application/json':
                 data = r.json()
                 #print('Recieved data: ', json.dumps(data, sort_keys=True, indent=4))
-            print(j)
+                processRepoData(data)
             j = j + 1
-                #if processData(data) is True:
-                    #print("Tis true!")
-                    #langdata = getUserStats(data)
-                    #postToFirebase(langdata)
+        prog = str(limit) + '/' + str(limit)
+        print("Done!")
 
-#firebase = firebase.FirebaseApplication(firebaseEndpoint, None)
 #result = firebase.get('/users', None)
 #print(result)
